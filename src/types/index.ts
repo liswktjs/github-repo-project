ff3bbe79c84b77a14007f7722862bec94f799026
JASUN LEE <liswktjs@gmail.com>
2023-01-19 20:18:46 +0900

    type: repo, issues 응답값 타입 정의

---
+++ src/types/index.ts
@@ -0,0 +1,41 @@
+export interface OwnerType {
+	login: string;
+	avatar_url: string;
+	html_url: string;
+}
+
+export interface SearchItemType {
+	id: number;
+	full_name: string;
+	owner: OwnerType;
+	html_url: string;
+	description: string;
+	has_issues: boolean;
+	open_issues_count: number;
+	updated_at: string;
+}
+
+export interface SearchResultType {
+	totalCount: number;
+	items: [];
+}
+
+export interface LabelItemType {
+	id: number;
+	name: string;
+	color: string;
+}
+
+export interface IssueItemType {
+	id: number;
+	html_url: string;
+	number: number;
+	title: string;
+	user: OwnerType;
+	labels: LabelItemType[];
+	state: string;
+	assignees: string[];
+	comments: number;
+	updated_at: string;
+	body: string;
+}
