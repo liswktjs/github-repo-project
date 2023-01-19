47a157290bf7bb93203475b7fef589430947e078
JASUN LEE <liswktjs@gmail.com>
2023-01-19 20:19:26 +0900

    feat: 검색된 Repo의 결과물을 보여줄 컴포넌트 구현

---
+++ src/components/RepoCard/RepoCard.stories.tsx
@@ -0,0 +1,30 @@
+import { Meta, Story } from '@storybook/react';
+import RepoCard, { RepoCardProps } from './RepoCard';
+
+export default {
+	title: 'RepoCard',
+	component: RepoCard,
+} as Meta;
+
+const Template: Story<RepoCardProps> = (args) => <RepoCard {...args} />;
+
+export const DefaultRepoCard = Template.bind({});
+DefaultRepoCard.args = {
+	repo: {
+		id: 508116421,
+		full_name: 'woowacourse-teams/2022-gong-seek',
+		owner: {
+			login: 'woowacourse-teams',
+			avatar_url: 'https://avatars.githubusercontent.com/u/66992148?v=4',
+			html_url: 'https://github.com/woowacourse-teams',
+		},
+		html_url: 'https://github.com/woowacourse-teams/2022-gong-seek',
+		description: '공식(공유해줘 너의 지식) : 우테코 크루들을 위한 질문 게시판',
+		has_issues: true,
+		open_issues_count: 35,
+		updated_at: '2022-12-01T02:23:09Z',
+	},
+	onRepoAddButtonClick: () => {
+		console.log('repo저장하는 버튼 클릭');
+	},
+};
---
+++ src/components/RepoCard/RepoCard.tsx
@@ -0,0 +1,96 @@
+import { Avatar, Badge, Card, IconButton } from '@mui/material';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+
+import styled from '@emotion/styled';
+
+import { SearchItemType } from '@/types';
+
+export interface RepoCardProps {
+	repo: SearchItemType;
+	onRepoAddButtonClick: () => void;
+}
+
+const RepoCard = ({ repo, onRepoAddButtonClick }: RepoCardProps) => {
+	return (
+		<Card sx={{ width: 500, height: 250 }}>
+			<RepoInfoContainer>
+				<RepoTitle>{repo.full_name}</RepoTitle>
+				<RepoDescription>{repo.description}</RepoDescription>
+			</RepoInfoContainer>
+
+			<UnderLineInfoContainer>
+				<UserInfoContainer>
+					<Avatar
+						alt="유저의 프로필"
+						src={repo.owner.avatar_url}
+						sx={{ width: 30, height: 30 }}
+					/>
+					<UserName>{repo.owner.login}</UserName>
+				</UserInfoContainer>
+
+				<ArticleInfoContainer>
+					<Badge badgeContent={repo.open_issues_count} color="primary">
+						<IssueContent>open issue</IssueContent>
+					</Badge>
+
+					<IconButton
+						aria-label="repository 저장하기"
+						onClick={onRepoAddButtonClick}
+					>
+						<AddCircleOutlineIcon sx={{ width: 25, height: 25 }} />
+					</IconButton>
+				</ArticleInfoContainer>
+			</UnderLineInfoContainer>
+		</Card>
+	);
+};
+
+const RepoInfoContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin: 0 20px 30px 20px;
+`;
+
+const RepoTitle = styled.h3``;
+
+const RepoDescription = styled.div`
+	min-height: 100px;
+	overflow: hidden;
+	text-overflow: ellipsis;
+`;
+
+const UnderLineInfoContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+`;
+
+const IssueContent = styled.div`
+	width: fit-content;
+	height: fit-content;
+`;
+
+const UserInfoContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 10px;
+	margin-left: 20px;
+`;
+
+const UserName = styled.div`
+	max-width: 150px;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	white-space: nowrap;
+`;
+
+const ArticleInfoContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 20px;
+
+	margin-right: 20px;
+`;
+
+export default RepoCard;
